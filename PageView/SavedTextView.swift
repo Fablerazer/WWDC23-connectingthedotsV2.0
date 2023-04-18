4d9ba467851ababeab76220121a7234cfa02fc60
Faizal H R <rizfianto@gmail.com>
2023-04-18 19:55:05 +0700

    Edit or update & delete

--- PageView/SavedTextView.swift
+++ PageView/SavedTextView.swift
@@ -9,12 +9,19 @@ import SwiftUI
 
 struct SavedTextsView: View {
     @EnvironmentObject var storage: TextStorage
-
+    @State private var editMode = EditMode.inactive
+    
     var body: some View {
         List {
             Section(header: Text("SD")) {
                 ForEach(storage.sdTexts, id: \.self) { text in
                     Text(text)
+                }
+                .onDelete { indexSet in
+                    storage.sdTexts.remove(atOffsets: indexSet)
+                }
+                .onMove { indices, newOffset in
+                    storage.sdTexts.move(fromOffsets: indices, toOffset: newOffset)
                 }
             }
 
@@ -22,11 +29,23 @@ struct SavedTextsView: View {
                 ForEach(storage.smpTexts, id: \.self) { text in
                     Text(text)
                 }
+                .onDelete { indexSet in
+                    storage.smpTexts.remove(atOffsets: indexSet)
+                }
+                .onMove { indices, newOffset in
+                    storage.smpTexts.move(fromOffsets: indices, toOffset: newOffset)
+                }
             }
 
             Section(header: Text("SMA")) {
                 ForEach(storage.smaTexts, id: \.self) { text in
                     Text(text)
+                }
+                .onDelete { indexSet in
+                    storage.smaTexts.remove(atOffsets: indexSet)
+                }
+                .onMove { indices, newOffset in
+                    storage.smaTexts.move(fromOffsets: indices, toOffset: newOffset)
                 }
             }
 
@@ -34,9 +53,17 @@ struct SavedTextsView: View {
                 ForEach(storage.kuliahTexts, id: \.self) { text in
                     Text(text)
                 }
+                .onDelete { indexSet in
+                    storage.kuliahTexts.remove(atOffsets: indexSet)
+                }
+                .onMove { indices, newOffset in
+                    storage.kuliahTexts.move(fromOffsets: indices, toOffset: newOffset)
+                }
             }
         }
         .navigationBarTitle("Saved Texts")
+        .navigationBarItems(trailing: EditButton())
+        .environment(\.editMode, $editMode)
     }
 }
 
