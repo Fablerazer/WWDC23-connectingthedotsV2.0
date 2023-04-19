6497c8b717b486ca469567771d509105731a6d6e
Faizal H R <rizfianto@gmail.com>
2023-04-19 11:08:57 +0700

    move to another solution, now we can count the mostfrequentword

--- PageView/SavedTextView.swift
+++ PageView/SavedTextView.swift
@@ -7,12 +7,61 @@
 
 import SwiftUI
 
+class DiscardedItems {
+    static let shared = DiscardedItems()
+    private init() {}
+    
+    var items: [String] = ["the", "and", "[T]","[L]","[T&L]"]
+}
+
+
 struct SavedTextsView: View {
     @EnvironmentObject var storage: TextStorage
     @State private var editMode = EditMode.inactive
     
+    private var discardedItems = DiscardedItems.shared.items
+    private var ignoreWords = ["the", "and", "[T]", "[L]", "[T&L]"]
+    
+    private var allItems: [String] {
+        storage.sdTexts + storage.smpTexts + storage.smaTexts + storage.kuliahTexts
+    }
+    
+    private var mostFrequentWords: [String] {
+        getMostFrequentWords(in: allItems, topN: 10, ignoreWords: ignoreWords)
+    }
+    
+    private func getMostFrequentWords(in items: [String], topN: Int, ignoreWords: [String]) -> [String] {
+        var wordsFrequency = [String: Int]()
+        for section in [storage.sdTexts, storage.smpTexts, storage.smaTexts, storage.kuliahTexts].flatMap({ $0 }) {
+            let words = section.components(separatedBy: .whitespacesAndNewlines)
+            for word in words {
+                if !ignoreWords.contains(word), !discardedItems.contains(word), !CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: word)) {
+                    wordsFrequency[word, default: 0] += 1
+                }
+            }
+        }
+        let sortedWords = wordsFrequency.sorted { $0.value > $1.value }
+        return Array(sortedWords.prefix(topN)).map { $0.key }
+    }
+    
+    
     var body: some View {
         List {
+            Section(header: Text("Most frequent words:")) {
+                let wordCounts = allItems.reduce(into: [String: Int]()) { counts, word in
+                    if !ignoreWords.contains(word), !discardedItems.contains(word), !CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: word)) {
+                        counts[word, default: 0] += 1
+                    }
+                }
+                ForEach(mostFrequentWords, id: \.self) { word in
+                    Text("\(word): \(wordCounts[word] ?? 0)")
+                }
+                .onAppear {
+                    // Call getMostFrequentWords on apperance
+                    let _ = mostFrequentWords
+                }
+            }
+            
             Section(header: Text("Before & After Elementary School (SD)")) {
                 ForEach(storage.sdTexts, id: \.self) { text in
                     Text(text)
@@ -24,7 +73,7 @@ struct SavedTextsView: View {
                     storage.sdTexts.move(fromOffsets: indices, toOffset: newOffset)
                 }
             }
-
+            
             Section(header: Text("Junior High School (SMP)")) {
                 ForEach(storage.smpTexts, id: \.self) { text in
                     Text(text)
@@ -36,7 +85,7 @@ struct SavedTextsView: View {
                     storage.smpTexts.move(fromOffsets: indices, toOffset: newOffset)
                 }
             }
-
+            
             Section(header: Text("Senior High School (SMA)")) {
                 ForEach(storage.smaTexts, id: \.self) { text in
                     Text(text)
@@ -48,7 +97,7 @@ struct SavedTextsView: View {
                     storage.smaTexts.move(fromOffsets: indices, toOffset: newOffset)
                 }
             }
-
+            
             Section(header: Text("University Untill Now (Kuliah)")) {
                 ForEach(storage.kuliahTexts, id: \.self) { text in
                     Text(text)
@@ -64,6 +113,7 @@ struct SavedTextsView: View {
         .navigationBarTitle("Saved Texts")
         .navigationBarItems(trailing: EditButton())
         .environment(\.editMode, $editMode)
+        
     }
 }
 
