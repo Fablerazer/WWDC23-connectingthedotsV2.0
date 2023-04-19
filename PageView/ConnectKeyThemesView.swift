6b8f132f292f99cdd0cfe799c854125d3d4dcf39
Faizal H R <rizfianto@gmail.com>
2023-04-19 10:16:42 +0700

    latest edit, ignore the error and find other solution

--- PageView/ConnectKeyThemesView.swift
+++ PageView/ConnectKeyThemesView.swift
@@ -17,6 +17,7 @@ struct Line {
 
 struct ConnectKeyThemesView: View {
     @EnvironmentObject var storage: TextStorage
+    
     @State private var lines = [Line]()
     @State private var selectedColor: Color = .black
     @State private var selectedLineWidth: CGFloat = 1
@@ -26,158 +27,163 @@ struct ConnectKeyThemesView: View {
     @State private var colorSwitcher : Bool = true
     
     var body: some View {
-        ZStack{
-            // First layer: canvas
-            Canvas { context, size in
-                for line in lines {
-                    var path = Path()
-                    path.addLines(line.points)
-                    context.stroke(path, with: .color(line.color), lineWidth: line.lineWidth)
+        VStack {
+            // Use filteredItems in your view
+            // Rest of your code
+            ZStack{
+                // First layer: canvas
+                Canvas { context, size in
+                    for line in lines {
+                        var path = Path()
+                        path.addLines(line.points)
+                        context.stroke(path, with: .color(line.color), lineWidth: line.lineWidth)
+                    }
+                }
+                .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local).onChanged({ value in
+                    let newPoint = value.location
+                    
+                    if colorSwitcher == true {
+                        if value.translation.width + value.translation.height == 0 {
+                            lines.append(Line(points: [newPoint], color: Color(.orange), lineWidth: 5))
+                        }else{
+                            let index = lines.count - 1
+                            lines[index].points.append(newPoint)
+                        }
+                    }else{
+                        if value.translation.width + value.translation.height == 0 {
+                            lines.append(Line(points: [newPoint], color: Color(.blue), lineWidth: 5))
+                        }else{
+                            let index = lines.count - 1
+                            lines[index].points.append(newPoint)
+                        }
+                    }
+                }))
+                HStack {
+                    VStack {
+                        VStack {
+                            // Texts for section 1
+                            Text("Before & After Elementary School (SD)")
+                                .foregroundColor(.black)
+                                .font(.title2)
+                            HStack {
+                                ForEach(storage.sdTexts, id: \.self) { text in
+                                    Text(text)
+                                        .foregroundColor(.black)
+                                        .font(.title2)
+                                        .padding()
+                                        .cornerRadius(10)
+                                }
+                            }
+                            Spacer()
+                        }
+                        
+                        VStack {
+                            // Texts for section 2
+                            Text("Junior High School (SMP)")
+                                .foregroundColor(.black)
+                                .font(.title2)
+                            HStack {
+                                ForEach(storage.smpTexts, id: \.self) { text in
+                                    Text(text)
+                                        .foregroundColor(.black)
+                                        .font(.title2)
+                                        .padding()
+                                        .cornerRadius(10)
+                                }
+                            }
+                            Spacer()
+                        }
+                        
+                        VStack {
+                            // Texts for section 3
+                            Text("Senior High School (SMA)")
+                                .foregroundColor(.black)
+                                .font(.title2)
+                            HStack {
+                                ForEach(storage.smaTexts, id: \.self) { text in
+                                    Text(text)
+                                        .foregroundColor(.black)
+                                        .font(.title2)
+                                        .padding()
+                                        .cornerRadius(10)
+                                }
+                            }
+                            Spacer()
+                        }
+                        
+                        VStack {
+                            // Texts for section 4
+                            Text("University Until Now (Kuliah)")
+                                .foregroundColor(.black)
+                                .font(.title2)
+                            HStack {
+                                ForEach(storage.kuliahTexts, id: \.self) { text in
+                                    Text(text)
+                                        .foregroundColor(.black)
+                                        .font(.title2)
+                                        .padding()
+                                        .cornerRadius(10)
+                                }
+                            }
+                            Spacer()
+                        }
+                        
+                        VStack(){
+                            
+                            // Action Button...
+                            if showNavigation == false {
+                                
+                                Button("Done Connecting?"){
+                                    showNavigation.toggle()
+                                    showReading.toggle()
+                                }
+                                .font(.title2.bold())
+                                .padding()
+                            }
+                            
+                            if showNavigation == true {
+                                
+                                // show text that you have to screenshot this so it would be saved to your device
+                                NavigationLink(destination: ManyShapedSkillView()) {
+                                    Text("Next Page")
+                                        .font(.title2.bold())
+                                        .foregroundColor(.white)
+                                        .padding(.vertical, 18)
+                                        .frame(width: 200)
+                                        .background(Color.black,in:
+                                                        RoundedRectangle(cornerRadius: 12))
+                                }
+                            }
+                        }
+                        
+                        .offset(y:-80)
+                    }
                 }
             }
-            .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local).onChanged({ value in
-                let newPoint = value.location
-                
-                if colorSwitcher == true {
-                    if value.translation.width + value.translation.height == 0 {
-                        lines.append(Line(points: [newPoint], color: Color(.orange), lineWidth: 5))
-                    }else{
-                        let index = lines.count - 1
-                        lines[index].points.append(newPoint)
-                    }
-                }else{
-                    if value.translation.width + value.translation.height == 0 {
-                        lines.append(Line(points: [newPoint], color: Color(.blue), lineWidth: 5))
-                    }else{
-                        let index = lines.count - 1
-                        lines[index].points.append(newPoint)
-                    }
-                }
-            }))
-            HStack {
-                VStack {
-                    VStack {
-                        // Texts for section 1
-                        Text("Before & After Elementary School (SD)")
-                            .foregroundColor(.black)
+            .overlay(
+                HStack{
+                    Button(action:{ lines = [Line]() }){
+                        Text("Reset Drawing")
                             .font(.title2)
-                        HStack {
-                            ForEach(storage.sdTexts, id: \.self) { text in
-                                Text(text)
-                                    .foregroundColor(.black)
-                                    .font(.title2)
-                                    .padding()
-                                    .cornerRadius(10)
-                            }
-                        }
-                        Spacer()
-                    }
-                    
-                    VStack {
-                        // Texts for section 2
-                        Text("Junior High School (SMP)")
-                            .foregroundColor(.black)
-                            .font(.title2)
-                        HStack {
-                            ForEach(storage.smpTexts, id: \.self) { text in
-                                Text(text)
-                                    .foregroundColor(.black)
-                                    .font(.title2)
-                                    .padding()
-                                    .cornerRadius(10)
-                            }
-                        }
-                        Spacer()
-                    }
-                    
-                    VStack {
-                        // Texts for section 3
-                        Text("Senior High School (SMA)")
-                            .foregroundColor(.black)
-                            .font(.title2)
-                        HStack {
-                            ForEach(storage.smaTexts, id: \.self) { text in
-                                Text(text)
-                                    .foregroundColor(.black)
-                                    .font(.title2)
-                                    .padding()
-                                    .cornerRadius(10)
-                            }
-                        }
-                        Spacer()
-                    }
-                    
-                    VStack {
-                        // Texts for section 4
-                        Text("University Until Now (Kuliah)")
-                            .foregroundColor(.black)
-                            .font(.title2)
-                        HStack {
-                            ForEach(storage.kuliahTexts, id: \.self) { text in
-                                Text(text)
-                                    .foregroundColor(.black)
-                                    .font(.title2)
-                                    .padding()
-                                    .cornerRadius(10)
-                            }
-                        }
-                        Spacer()
-                    }
-                    
-                    VStack(){
-                        
-                        // Action Button...
-                        if showNavigation == false {
-                            
-                            Button("Done Connecting?"){
-                                showNavigation.toggle()
-                                showReading.toggle()
-                            }
-                            .font(.title2.bold())
+                            .foregroundColor(.red)
                             .padding()
-                        }
-                        
-                        if showNavigation == true {
-                            
-                            // show text that you have to screenshot this so it would be saved to your device
-                            NavigationLink(destination: ManyShapedSkillView()) {
-                                Text("Next Page")
-                                    .font(.title2.bold())
-                                    .foregroundColor(.white)
-                                    .padding(.vertical, 18)
-                                    .frame(width: 200)
-                                    .background(Color.black,in:
-                                                    RoundedRectangle(cornerRadius: 12))
-                            }
-                        }
                     }
-                    
-                    .offset(y:-80)
-                    .navigationBarTitle("Connect The Dots!")
+                    Button(action:{ colorSwitcher.toggle() }){
+                        Text("Change Color")
+                            .font(.title2)
+                            .foregroundColor(.blue)
+                            .padding()
+                    }
                 }
-            }
+                
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
+                ,alignment: .bottom
+            )
+            .navigationBarTitle("Connect The Dots!")
+            
         }
-        .overlay(
-            HStack{
-                Button(action:{ lines = [Line]() }){
-                    Text("Reset Drawing")
-                        .font(.title2)
-                        .foregroundColor(.red)
-                    
-                    Button("Change Color"){
-                        colorSwitcher.toggle()
-                    }
-                    .font(.title2.bold())
-                    .foregroundColor(.blue)
-                    
-                    .padding()
-                    
-                    
-                }
-            }
-                .padding(EdgeInsets(top: 0, leading: 0, bottom: 15, trailing: 0))
-            ,alignment: .bottom
-        )
+        
+        
     }
 }
 
